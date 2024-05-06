9cdaef15ef217edb8e71bd2936e9b08316c37785
Paulo <paulorfsantos17@gmail.com>
2024-05-06 09:20:48 -0300

    ➜  fix: rename column table username

---
+++ prisma/migrations/20240503184003_rename_col_usernames_for_username/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `usernames` on the `users` table. All the data in the column will be lost.
+  - Added the required column `username` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "username" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO "new_users" ("created_at", "id", "name") SELECT "created_at", "id", "name" FROM "users";
+DROP TABLE "users";
+ALTER TABLE "new_users" RENAME TO "users";
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
